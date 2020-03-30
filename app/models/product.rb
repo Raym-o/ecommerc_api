5ac29fb303076e39a024d8c54bc66ff0bd9dba5c
Raymond Weiss <raymondaweiss@gmail.com>
2020-03-30 18:14:18 -0500

    Products Model, Added validations and commentedblocked code from michaelvasallo github Atrium for future modelling

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,15 @@ class Product < ApplicationRecord
   has_many :order_products
   has_many :collection_products
   has_many :collections, through: :collection_products
+
+  validates :title, :description, :price, presence: true
+  validates :title, uniqueness: true
+
+  # def on_sale?
+  #   discount != 0
+  # end
+
+  # def sale_price
+  #   on_sale? ? (price - price * discount).round(2) : price
+  # end
 end
