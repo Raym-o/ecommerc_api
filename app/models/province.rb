8aa92b90c568936c368d7f3f2a3f34ee3d9844f0
Raymond Weiss <raymondaweiss@gmail.com>
2020-03-30 18:16:18 -0500

    Added validation to Province model

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Province < ApplicationRecord
-  has_many :addresses
+  has_many :addresses, dependent: :restrict_with_error
   has_many :customers, through: :addresses
+
+  validates :name, presence: true, uniqueness: true
 end
