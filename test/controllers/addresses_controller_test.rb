bb0850d13785730f19555a8c7406fe2031c50920
Raymond Weiss <raymondaweiss@gmail.com>
2020-03-30 16:37:53 -0500

    Scaffolded Address, removed html view items

---
+++ test/controllers/addresses_controller_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class AddressesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @address = addresses(:one)
+  end
+
+  test "should get index" do
+    get addresses_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_address_url
+    assert_response :success
+  end
+
+  test "should create address" do
+    assert_difference('Address.count') do
+      post addresses_url, params: { address: { city: @address.city, customer_id: @address.customer_id, line_1: @address.line_1, line_2: @address.line_2, postal_code: @address.postal_code, province_id: @address.province_id } }
+    end
+
+    assert_redirected_to address_url(Address.last)
+  end
+
+  test "should show address" do
+    get address_url(@address)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_address_url(@address)
+    assert_response :success
+  end
+
+  test "should update address" do
+    patch address_url(@address), params: { address: { city: @address.city, customer_id: @address.customer_id, line_1: @address.line_1, line_2: @address.line_2, postal_code: @address.postal_code, province_id: @address.province_id } }
+    assert_redirected_to address_url(@address)
+  end
+
+  test "should destroy address" do
+    assert_difference('Address.count', -1) do
+      delete address_url(@address)
+    end
+
+    assert_redirected_to addresses_url
+  end
+end
