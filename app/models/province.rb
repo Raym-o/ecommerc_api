30c7c37239c5b09795459ab12d1f894e19dd7137
Raymond Weiss <raymondaweiss@gmail.com>
2020-03-30 21:25:20 -0500

    Added to seed.rb file

--- app/models/province.rb
+++ app/models/province.rb
@@ -4,6 +4,7 @@ class Province < ApplicationRecord
   has_many :addresses, dependent: :restrict_with_error
   has_many :customers, through: :addresses
 
-  validates :name, :abbr, presence: true, uniqueness: true
+  validates :name, :abbr, :pst_rate, :hst_rate, presence: true
+  validates :name, :abbr, uniqueness: true
   validates :abbr, length: { minimum: 2, maximum: 2 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+provinces = [
+  { name: 'Alberta', abbr: 'AB', pst_rate: 0, hst_rate: 0 },
+  { name: 'British Columbia', abbr: 'BC', pst_rate: 0.07, hst_rate: 0 },
+  { name: 'Manitoba', abbr: 'MB', pst_rate: 0.07, hst_rate: 0 },
+  { name: 'New Brunswick', abbr: 'NB', pst_rate: 0, hst_rate: 0.1 },
+  { name: 'Newfoundland and Labrador', abbr: 'NL', pst_rate: 0, hst_rate: 0.1 },
+  { name: 'Nova Scotia', abbr: 'NS', pst_rate: 0, hst_rate: 0.1 },
+  { name: 'Northwest Territories', abbr: 'NT', pst_rate: 0, hst_rate: 0 },
+  { name: 'Nunavut', abbr: 'NU', pst_rate: 0, hst_rate: 0 },
+  { name: 'Ontario', abbr: 'ON', pst_rate: 0, hst_rate: 0.08 },
+  { name: 'Prince Edward Island', abbr: 'PE', pst_rate: 0, hst_rate: 0.1 },
+  { name: 'Quebec', abbr: 'QC', pst_rate: 0.09975, hst_rate: 0 },
+  { name: 'Saskatchewan', abbr: 'SK', pst_rate: 0.06, hst_rate: 0 },
+  { name: 'Yukon', abbr: 'YT', pst_rate: 0, hst_rate: 0 }
+]
+
+provinces.each do |province|
+  prov = Province.create(
+    name: province[:name],
+    abbr: province[:abbr],
+    pst_rate: province[:pst_rate],
+    hst_rate: province[:hst_rate]
+  )
+end
+
+10.times do
+  product = Product.create(
+    title: Faker::Food.unique.dish,
+    description: Faker::Food.unique.description,
+    price: Faker::Number.decimal(l_digits: 2, r_digits: 2)
+  )
+end
