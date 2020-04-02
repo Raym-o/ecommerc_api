568aa36cad403726686e66e2f6c77d1a0695b796
Raymond Weiss <raymondaweiss@gmail.com>
2020-04-02 17:05:18 -0500

    Added basic length limit validations to Page title and content properties

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class Page < ApplicationRecord
+  validates :title, presence: true
+
+  validates :title, length: { maximum: 50 }
+
+  validates :content, length: { maximum: 2500 }
 end
